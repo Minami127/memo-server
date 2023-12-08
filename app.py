4e2bd6e8121c698d34b7ccba5adddba8c0d79a3b
Minami127 <jws282288@gmail.com>
2023-12-08 11:30:38 +0900

    s

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from config import Config
 
 from resources.user import UserRegisterResource
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+jwt = JWTManager(app)
 
 api = Api(app)
 
